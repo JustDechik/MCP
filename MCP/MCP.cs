310369e84c3aa446623948bc2886baeeda7d1742
strawberry.jam.from.russia@gmail.com <strawberry.jam.from.russia@gmail.com>
2019-11-18 07:27:44 +0000

    Add rotate function

---
+++ MCP/MCP.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class MCP
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("MCP\n");
+
+
+    }
+
+    
+    private static double Rotate(int[] A, int[] B, int[] C)
+    {
+        // z > 0 left else right
+        return ((B[0] - A[0]) * (C[1] - B[1])) - ((B[1] - A[1]) * (C[0] - B[0]));
+    }
+}
+
